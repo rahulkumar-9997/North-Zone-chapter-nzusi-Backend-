b681dad2208902e08f85352b5775f55e16259b4a
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2026-04-30 14:40:53 +0530

    blog controller

--- app/Http/Controllers/Backend/BlogPostController.php
+++ app/Http/Controllers/Backend/BlogPostController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\BlogCategory;
+use App\Models\Blog;
 
 class BlogPostController extends Controller
 {
-    //
+    public function index()
+    {
+        $blogCategories = BlogCategory::orderBy('id', 'desc')->get();
+        return view('backend.pages.blog.index', compact('blogCategories'));
+    }
 }
---
+++ resources/views/backend/pages/blog/index.blade.php
@@ -0,0 +1,57 @@
+@extends('backend.layouts.master')
+@section('title','Blog List')
+@push('styles')
+@endpush
+@section('main-content')
+<div class="content">
+    <div class="card">
+        <div class="card-header d-flex align-items-center justify-content-between flex-wrap row-gap-3">
+            <h4 class="card-title">Blog List</h4>
+            <a href="{{ route('blog-post.create') }}" 
+            class="btn btn-primary">
+                <i data-feather="plus" class="me-2"></i> Add New Blog
+            </a>           
+        </div>
+        <div class="card-body p-0">
+            <div class="table-responsive">
+                @if($blogCategories->count() > 0)
+                <div class="blog-category-list-table-render">
+                    @include('backend.pages.blog.partials.category-list', ['blogCategories' => $blogCategories])
+                </div>
+                @else
+                <div class="text-center p-4">
+                    <h4 class="mb-2">No Blog Found</h4>
+                    <p class="mb-0">Start creating your first blog category.</p>
+                </div>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+@push('scripts')
+<script src="{{ asset('backend/assets/js/pages/blog-category.js ') }}"></script>
+<script>
+   $(document).ready(function() {
+        $('.show_confirm').click(function(event) {
+         var form = $(this).closest("form");
+         var name = $(this).data("name");
+         event.preventDefault(); 
+            Swal.fire({
+                title: `Are you sure you want to delete this ${name}?`,
+                text: "If you delete this, it will be gone forever.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Yes, delete it!",
+                cancelButtonText: "Cancel",
+                dangerMode: true,
+            }).then((result) => {
+               if (result.isConfirmed) {
+                  form.submit();
+               }
+            });
+        });           
+    });
+</script>
+@endpush
